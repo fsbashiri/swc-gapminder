0b9e72594bdcd8ec83a813138cf47e75266b79df
Azi Bashiri <fsbashiri@gmail.com>
2022-01-21 11:57:57 -0600

    Allowing plot generation for multiple files at once

--- gdp_plots.py
+++ gdp_plots.py
@@ -4,21 +4,21 @@ import pandas
 # because we are no longer in a notebook
 import matplotlib.pyplot as plt
 
-filename = sys.argv[1]
+# filename = sys.argv[1]
+for filename in sys.argv[1:]:
+    # load data and transpose so that country names are
+    # the columns and their gdp data becomes the rows
+    data = pandas.read_csv(filename, index_col = 'country').T
 
-# load data and transpose so that country names are
-# the columns and their gdp data becomes the rows
-data = pandas.read_csv(filename, index_col = 'country').T
+    # create a plot of the transposed data
+    ax = data.plot(title = filename)
 
-# create a plot of the transposed data
-ax = data.plot(title = filename)
+    # set some plot attributes
+    ax.set_xlabel("Year")
+    ax.set_ylabel("GDP Per Capita")
+    # set the x location and labels
+    ax.set_xticks(range(len(data.index)))
+    ax.set_xticklabels(data.index, rotation = 45)
 
-# set some plot attributes
-ax.set_xlabel("Year")
-ax.set_ylabel("GDP Per Capita")
-# set the x location and labels
-ax.set_xticks(range(len(data.index)))
-ax.set_xticklabels(data.index, rotation = 45)
-
-# display the plot
-plt.show()
+    # display the plot
+    plt.show()
